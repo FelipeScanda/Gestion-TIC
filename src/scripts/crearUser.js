74fa26c138691f51cdd15fb2e35c3f194abbfb5a
Felipe Scanda <felipescanda@gmail.com>
2026-03-25 10:49:49 -0300

    Script para crear usuarios en db

--- src/scripts/crearUser.js
+++ src/scripts/crearUser.js
@@ -1,34 +1,79 @@
-import bcrypt from "bcrypt";
+import fs from "fs";
+import csv from "csv-parser";
 import prisma from "../config/prisma.js";
+import bcrypt from "bcrypt";
 
-const run = async () => {
-  try {
-    const nombre = "User 2";
-    const rut = "33333333-3";
-    const email = "user2@mail.com";
-    const plainPassword = "user123";
-    const rol_usuario = "USER";
+const resultados = [];
 
-    const password = await bcrypt.hash(plainPassword, 10);
+// Limpia acentos y pasa a minúscula
+const limpiar = (str) =>
+  str
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
 
-    const user = await prisma.usuario.create({
-      data: {
-        nombre,
-        rut,
-        email,
-        password: password,
-        rol_usuario,
-        estado: "ACTIVO",
-      },
-    });
+// Genera email: nombre.apellido@dominio.com
+const generarEmail = (nombreCompleto) => {
+  const partes = nombreCompleto.trim().split(/\s+/);
 
-    console.log("Usuario creado correctamente:");
-    console.log(user);
-  } catch (error) {
-    console.error("Error creando usuario:", error.message);
-  } finally {
-    await prisma.$disconnect();
+  if (partes.length < 3) {
+    throw new Error(`Formato inválido: ${nombreCompleto}`);
   }
+
+  const apellido1 = limpiar(partes[0]);
+  const nombre1 = limpiar(partes[2]);
+
+  return `${nombre1}.${apellido1}@apssanvicente.cl`;
 };
 
-run();
+// Leer CSV
+fs.createReadStream("usuarios.csv")
+  .pipe(csv())
+  .on("data", (data) => resultados.push(data))
+  .on("end", async () => {
+    try {
+      const hashedPassword = await bcrypt.hash("Cesfam2026", 10);
+
+      for (const user of resultados) {
+        try {
+          const nombreCompleto = user.nombre_completo.trim();
+          const rut = user.rut.trim();
+
+          const email = generarEmail(nombreCompleto);
+
+          // evitar duplicados por email
+          const existe = await prisma.usuario.findUnique({
+            where: { rut }
+          });
+
+          if (existe) {
+            console.log(`Usuario ya existe: ${email} ${rut}`);
+            continue;
+          }
+
+          await prisma.usuario.create({
+            data: {
+              nombre: nombreCompleto,
+              rut: rut,
+              email: email,
+              password: hashedPassword,
+              rol_usuario: "USER",
+              estado: "ACTIVO"
+            }
+          });
+
+          console.log(`Usuario creado: ${email}`);
+
+        } catch (errorInterno) {
+          console.error("Error con usuario:", user, errorInterno.message);
+        }
+      }
+
+      console.log("Importación finalizada");
+
+    } catch (error) {
+      console.error("Error general:", error);
+    } finally {
+      await prisma.$disconnect();
+    }
+  });
